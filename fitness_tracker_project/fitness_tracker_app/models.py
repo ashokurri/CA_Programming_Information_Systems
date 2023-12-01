6b923f4c8e8dbffef55609dcb6453eeb9f140451
ashokurri <150040092+ashokurri@users.noreply.github.com>
2023-12-01 16:35:32 +0000

    created a model "Excercise"
    created a model "Excercise" and performed migration to the sqlite database

--- fitness_tracker_project/fitness_tracker_app/models.py
+++ fitness_tracker_project/fitness_tracker_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    calories_burned = models.FloatField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+
