a86d8be75a6b3f8b5aaf062c1a2b9826ca028575
sudhakar1502 <149905670+sudhakar1502@users.noreply.github.com>
2023-12-04 22:12:05 +0000

    added django views
    added django views for adding and deleting excercise

--- fitness_tracker_project/fitness_tracker_app/views.py
+++ fitness_tracker_project/fitness_tracker_app/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Exercise
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def exercise_list(request):
     exercises = Exercise.objects.all()
     data = [{'name': exercise.name, 'duration': exercise.duration, 'calories_burned': exercise.calories_burned, 'date': exercise.date} for exercise in exercises]
     return JsonResponse(data, safe=False)
+
+@csrf_exempt
+def add_exercise(request):
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        new_exercise = Exercise.objects.create(
+            name=body['name'],
+            duration=body['duration'],
+            calories_burned=body['calories'],
+            date=body['date']
+        )
+        return JsonResponse({'message': 'Exercise added successfully'})
+    return JsonResponse({'message': 'Failed to add exercise'}, status=400)
+
+@csrf_exempt
+def delete_exercise(request, exercise_id):
+    try:
+        exercise = Exercise.objects.get(pk=exercise_id)
+        exercise.delete()
+        return JsonResponse({'message': 'Exercise deleted successfully'})
+    except Exercise.DoesNotExist:
+        return JsonResponse({'message': 'Exercise does not exist'}, status=404)
